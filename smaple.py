897fbb2f6d6c2765ef9b8bc768d36bfe5a62e56c
srujithr <srujithrajan001@gmail.com>
2024-01-31 18:30:13 +0530

    first commit

---
+++ smaple.py
@@ -0,0 +1,48 @@
+ <div class="left">
+            <section class="copy">
+                <h1>Explore your Creaticity</h1>
+                <p>over 1000 course taugt by real Creaticity.</p>
+            </section>
+        </div>
+
+
+
+
+
+
+
+        <div class="right">
+            <form>
+                <section class="copy">
+                    <h2>Sign Up</h2>
+                    <div class="login-container">
+                        <p>Already have and Account? <a href="#"><strong>Log In</strong></a></p>
+                    </div>
+                </section>
+                <div class="input-container name">
+                    <label for="fname">Full Name</label>
+                    <input for="fname" name="fname" type="text">
+                </div>
+                <div class="input-container Email">
+                    <label for="email">Email</label>
+                    <input for="email" name="email" type="text">
+                </div>
+                <div class="input-container password">
+                    <label for="password">password</label>
+                    <input for="password" name="password" placeholder="Must be at 6 characters" type="text">
+                    <i class="far fa-eye-slash"></i>
+                </div>
+                <div class="input-container cta">
+                    <label for="checkbox-container">
+                        <input type="checkbox">
+                        <span class="checkmark">
+                            sign up for email updates</span>
+                    </label>
+                </div>
+                <button class="signup-btn" type="submit">Sign Up </button>
+                <section class="copy legal">
+                    <p><span class="small">By Continuing, you agree to accept our br <a href="#">Privacy Policy</a>&amp;<a href="#">Terms of Service</a> </span></p>
+                </section>
+            </form>
+        </div>
+    </div>    
